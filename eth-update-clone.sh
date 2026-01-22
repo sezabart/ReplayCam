554d715dba0fcb6757498d203ad30a4b02b6da38
Bart Smits <bart@seza.si>
2026-01-22 17:05:15 +0100

    Simplify update script

--- eth-update-clone.sh
+++ eth-update-clone.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 
 # Wait until Ethernet is connected (e.g., eth0 or usb0)
-if ip link show eth0 | grep -q 'state UP'; do
-    
-
+if ip link show eth0 | grep -q 'state UP'; then
     # Perform apt update and upgrade
     sudo apt update && sudo apt upgrade -y
 
-    # Clone the repository if not already present
-    if [ ! -d "/home/pi/ReplayCam" ]; then
-        cd /home/pi
-        git clone https://github.com/sezabart/ReplayCam.git
-    else
-        cd /home/pi/ReplayCam
-        git pull origin main
-    fi
+    cd ~/ReplayCam
+    git pull origin main
+    
 
     # Optionally, install Python dependencies if requirements.txt exists
-    if [ -f "/home/pi/ReplayCam/requirements.txt" ]; then
-        sudo pip3 install -r /home/pi/ReplayCam/requirements.txt
+    if [ -f "~/ReplayCam/requirements.txt" ]; then
+        sudo pip3 install -r ~/ReplayCam/requirements.txt
     fi
 fi
